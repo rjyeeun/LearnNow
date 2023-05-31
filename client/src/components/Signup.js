f24f421d255c0357798b57626df9d840fa27ee3e
Jay T <jaytula@gmail.com>
2023-05-31 12:07:06 -0700

    Remove unused Image import

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
-import Image from 'react-bootstrap/Image'
 import {Link} from 'react-router-dom'
 
 
